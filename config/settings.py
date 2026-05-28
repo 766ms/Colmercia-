6d8ca856fcb4cda5b5167919833963cae2d2499e
766ms <soflovesarianaet@outlook.com>
2026-05-27 21:02:25 -0500

    fix collectstatic vercel

--- config/settings.py
+++ config/settings.py
@@ -179,6 +179,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ─────────────────────────────────────────────
 # MEDIA (IMÁGENES)
